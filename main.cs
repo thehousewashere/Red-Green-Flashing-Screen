293f5ae2541f66d67a8d08c2dcf297072ee560c2
Jacob Thomas <jacobmthomas9000@gmail.com>
2020-11-09 00:29:21 -0600

    Moved from Repl to my computer

--- main.cs
+++ main.cs
@@ -1,8 +1,8 @@
 using System;
 
 class MainClass {
-  public static void Main (string[] args) { 
-    
+  public static void Main (string[] args) {
+
     //Title Screen
     Console.ForegroundColor = ConsoleColor.Green;
     Console.Write("Red");
@@ -11,16 +11,16 @@ class MainClass {
     Console.ForegroundColor = ConsoleColor.Red;
     Console.Write("Green");
     Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine(" Flashing Screen (V.1)");
+    Console.WriteLine(" Flashing Screen (V.1.1)");
     Console.WriteLine("By thehousewashere");
     System.Threading.Thread.Sleep(5000);
-    
+
     //Red and Green
     while (1 == 1) {
-      Console.BackgroundColor = ConsoleColor.Red;  
+      Console.BackgroundColor = ConsoleColor.Red;
       System.Threading.Thread.Sleep(100);
       Console.Clear();
-      Console.BackgroundColor = ConsoleColor.Green; 
+      Console.BackgroundColor = ConsoleColor.Green;
       System.Threading.Thread.Sleep(100);
       Console.Clear();
     }
